95f517f14c2f571a3c90f2865e349bd65cec158d
yazan hasan <yazan@aratech.ae>
2022-06-03 11:35:47 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
+};
+
+export default App;
